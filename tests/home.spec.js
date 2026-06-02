167526bab972380b47158b18319c7bc2780e56dd
CrunchyByte <104239065+CrunchyByte@users.noreply.github.com>
2026-06-02 10:19:49 -0700

    Update home.spec.js

--- tests/home.spec.js
+++ tests/home.spec.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('homepage loads', async ({ page }) => {
-  await page.goto('https://dev-portfolio.azurestaticapps.net');
+  await page.goto('https://zealous-field-04ce67510.7.azurestaticapps.net');
 
   await expect(page).toHaveTitle(/Dev Portfolio/);
 });
