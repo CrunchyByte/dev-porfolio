e61738ae3b00c2c3d98a6942182153a87647b9ad
CrunchyByte <104239065+CrunchyByte@users.noreply.github.com>
2026-06-02 10:39:18 -0700

    Update home.spec.js

--- tests/home.spec.js
+++ tests/home.spec.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('homepage loads', async ({ page }) => {
-  const baseURL = process.env.BASE_URL || 'https://dev-portfolio.azurestaticapps.net';
+  const baseURL = process.env.BASE_URL || 'https://zealous-field-04ce67510.7.azurestaticapps.net';
 
   await page.goto(baseURL);
 
